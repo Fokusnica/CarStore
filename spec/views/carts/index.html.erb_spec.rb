5a829c78dd07fe3d03cdf62eeb10d54819ca44f0
Voronoy Alexandr <servermed@gmail.com>
2015-08-18 15:52:02 +0300

    delete wrong spec

--- spec/views/carts/index.html.erb_spec.rb
+++ spec/views/carts/index.html.erb_spec.rb
@@ -1,14 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe "carts/index", type: :view do
-  before(:each) do
-    assign(:carts, [
-      Cart.create!(),
-      Cart.create!()
-    ])
-  end
-
-  it "renders a list of carts" do
-    render
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
--- spec/views/line_items/index.html.erb_spec.rb
+++ spec/views/line_items/index.html.erb_spec.rb
@@ -1,22 +1,6 @@
 require 'rails_helper'
 
 RSpec.describe "line_items/index", type: :view do
-  before(:each) do
-    assign(:line_items, [
-      LineItem.create!(
-        :product => nil,
-        :cart => nil
-      ),
-      LineItem.create!(
-        :product => nil,
-        :cart => nil
-      )
-    ])
-  end
 
-  it "renders a list of line_items" do
-    render
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-    assert_select "tr>td", :text => nil.to_s, :count => 2
-  end
+  pending "add some examples to (or delete) #{__FILE__}"
 end
