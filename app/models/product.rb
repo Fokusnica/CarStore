4cfac5c4b02d7c651591ba3957a67e896b44bd55
Voronoy Alexandr <servermed@gmail.com>
2015-08-18 12:14:28 +0300

    model cart and template

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,18 @@
 class Product < ActiveRecord::Base
+  before_destroy :product_not_have_line_items
   belongs_to :category
   has_many :products_providers, dependent: :destroy
   has_many :providers, through: :products_providers
+  has_many :line_items
+
+
+  def product_not_have_line_items
+    if line_items.empty?
+      return true
+    else
+      errors.add(:base, 'Существуют товарные позиции')
+      return false
+    end
+  end
+
 end
