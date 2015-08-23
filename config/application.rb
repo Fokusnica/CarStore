39887690f26cc44374fa155b3cbceb0f5aee2242
Liubov Derevianko <dereviankoliubov@gmail.com>
2015-08-23 17:01:32 +0300

    localization en config

--- config/application.rb
+++ config/application.rb
@@ -18,7 +18,7 @@ module CarStore
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
-     config.i18n.default_locale = :ru
+     config.i18n.default_locale = :en
      config.assets.paths << Rails.root.join("app", "assets", "fonts", "js", "images")
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
