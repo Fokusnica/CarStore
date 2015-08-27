fd7db1f71d2803b3e6c515624503dbeeac537d41
Mykyta Troianov <nicktroyanov@gmail.com>
2015-08-27 17:47:58 +0300

    fixed path to index page for travis

--- spec/features/index_page_spec.rb
+++ spec/features/index_page_spec.rb
@@ -3,8 +3,20 @@ require 'rails_helper'
 describe 'index page', js: true do
 
   it 'should have correct company email' do
-    visit ''
+    visit root_path
     expect(page).to have_content('octohellstore@gmail.com')
   end
+
+=begin
+  pending 'should open personal page' do
+  end
+
+  pending 'should have slider' do
+  end
+
+  pending 'should have categories' do
+  end
+=end
+
 
 end
